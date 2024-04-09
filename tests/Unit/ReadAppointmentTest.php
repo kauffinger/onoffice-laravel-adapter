da72a73c448db0334a0f1a2b215a8bbb0249c748
Bruno Görß <bruno.goerss@inno-brain.de>
2024-04-09 18:38:08 +0200

    test: add fake response to read appoinment test

--- tests/Unit/ReadAppointmentTest.php
+++ tests/Unit/ReadAppointmentTest.php
@@ -7,6 +7,8 @@ use Kauffinger\OnOfficeApi\Actions\Action;
 use Kauffinger\OnOfficeApi\Actions\ReadActions\ReadAppointmentAction;
 use Kauffinger\OnOfficeApi\Facades\OnOfficeApi;
 use Kauffinger\OnOfficeApi\OnOfficeApiRequest;
+use Saloon\Http\Faking\MockResponse;
+use Saloon\Laravel\Saloon;
 
 it('can be retrieved from Action base class', function () {
     $instance = Action::read()->appointment();
@@ -39,6 +41,10 @@ it('will send a successful request', function () {
             ->showCancelled()
             ->allUsers()
     );
+
+    Saloon::fake([
+        MockResponse::make([]),
+    ]);
 
     $response = OnOfficeApi::send($request);
 
